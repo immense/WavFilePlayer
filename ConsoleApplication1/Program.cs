e25fd23f2894cc46c8b9c8a65fefd437c5c7ba8d
Chris McKnight <cmckni3@gmail.com>
2016-04-06 20:55:20 -0500

    Adds sample code

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using NAudio.Wave;
+using System;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private static string wav_file_path = @".\test.wav";
+
+        static void Main(string[] args)
+        {
+            FileStream wav_file_stream = File.OpenRead(wav_file_path);
+
+            // Reset the stream to the beginning
+            wav_file_stream.Seek(0, SeekOrigin.Begin);
+
+            WaveStream ws_raw = new WaveFileReader(wav_file_stream);
+            ws_raw = WaveFormatConversionStream.CreatePcmStream(ws_raw);
+            WaveOutEvent output = new WaveOutEvent();
+            output.Init(ws_raw);
+            output.Play();
+
+            Console.Write("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# WavFilePlayer
+
+Sample C# .Net console application that shows how to use [NAudio](https://www.nuget.org/packages/NAudio/) to play a wav file.
+
+If you use the code in a GUI application, use `NAudio.Wave.WaveOut` instead of `NAudio.Wave.WaveOutEvent`.
